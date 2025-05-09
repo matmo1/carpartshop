a79a465393c023e7f6ebcfea14d94d168f8f4d8c
matmo1 <martin060607@gmail.com>
2025-05-09 12:38:45 +0300

    Start of the project

---
+++ src/main/java/com/mt/carpartshop/CarpartshopApplication.java
@@ -0,0 +1,13 @@
+package com.mt.carpartshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpartshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarpartshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mt/carpartshop/CarpartshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mt.carpartshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarpartshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
